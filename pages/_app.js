3178378fad59c7a1779fd3772caadcd8da2f6f9b
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2021-12-04 17:28:18 -0500

    netlify cms

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,20 @@
 import 'tailwindcss/tailwind.css';
 
+import { useEffect } from 'react';
+
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if (window.netlifyIdentity) {
+      window.netlifyIdentity.on('init', user => {
+        if (!user) {
+          window.netlifyIdentity.on('login', () => {
+            document.location.href = '/admin/';
+          });
+        }
+      });
+    }
+  });
+
   return <Component {...pageProps} />;
 }
 
