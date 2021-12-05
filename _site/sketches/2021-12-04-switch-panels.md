8132ca1ca912470d430645c512c7b2299ba5dc0a
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2021-12-04 19:11:48 -0500

    adds graphics page and fixes cms format

--- _site/sketches/2021-12-04-switch-panels.md
+++
@@ -1,4 +0,0 @@
----
-title: Switch Panels
-iframe-url: <iframe src="https://editor.p5js.org/sventi555/full/deB6UeL6C"></iframe>
----
---
+++ pages/graphics.tsx
@@ -0,0 +1,54 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+
+import Head from 'next/head';
+import YAML from 'yaml';
+
+import Footer from '../components/footer';
+import Navbar from '../components/navbar';
+
+// TODO change date type to specifically a moment.js string
+interface Sketch {
+  slug: string,
+  title: string,
+  'iframe-url': string,
+  screenshot: string,
+  date: string
+}
+
+interface GraphicsProps {
+  sketches: [Sketch]
+}
+
+export default function Graphics({sketches}: GraphicsProps) {
+  return (
+    <div>
+      <Head>
+        <title>Stephen Edwards</title>
+      </Head>
+      <Navbar />
+      <main>
+        {sketches.map((sketch) => (
+          <div key={sketch.slug}>
+            <p>{sketch.title}</p>
+          </div>
+        ))}
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const sketches = [];
+
+  const sketchDir = await fs.readdir(path.join(process.cwd(), '_site/sketches/'));
+  for (const sketchPath of sketchDir) {
+    const sketchData = YAML.parse(await fs.readFile(path.join(process.cwd(), `_site/sketches/${sketchPath}`), 'utf8'));
+    sketches.push({...sketchData, slug: sketchPath});
+  }
+
+  return {
+    props: { sketches }
+  };
+}
